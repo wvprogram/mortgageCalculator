b08acedd5428a40aa1a850f7b191b2457c7ec9cc
Wa Vang <wvprogram1983@gmail.com>
2023-05-11 11:16:44 -0500

    I made updates to the js file to capture and store the price and loan year input values

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-//  object to get all the number data from the html
+/*****
+  object to get and hold all the number input data from the html
+*****/
 let state = {
     price: getNumber(document.querySelectorAll('[name="price"]')[0].value),
     loan_years: document.querySelectorAll('[name="loan_years"]')[0].value,
@@ -9,7 +11,10 @@ let state = {
     hoa: document.querySelectorAll('[name="hoa"]')[0].value,
 }
 
-//  variables
+
+/***** 
+  variables 
+*****/
 let totalLoan,
     totalMonths,
     monthlyInterest,
@@ -40,31 +45,78 @@ let totalLoan,
         'rgb(255, 159, 64, 1)',
     ]
 
-//  function to turn the html number data to actual numbers
+
+/*****
+  function to turn the html number input data to actual numbers 
+*****/
 function getNumber(str) {
     return Number(str.replace(/[^0-9\.-]+/g, ""));
 }
 
-//  script to create the visual myChart
+
+/***** 
+  script to create the visual myChart 
+*****/
 const ctx = document.getElementById('myChart');
 
-new Chart(ctx, {
-  type: 'bar',
+let myChart = new Chart(ctx, {
+  //  type of chart
+  type: 'doughnut',
+
+  //  data to display in chart
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    //  labels created above in variable
+    labels: labels,
     datasets: [{
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      //  data taken from html input fields variables
+      data: [
+        monthlyPrincipalInterest,
+        monthlyPropertyTaxes,
+        monthlyHomeInsurance,
+        monthlyHOA
+      ],
+      //  background color and bordercolor taken from variable colors
+      backgroundColor: backgroundColor,
+      borderColor: borderColor,
       borderWidth: 1
     }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
   }
 });
 
-console.log(state);
+//  turn off chart animation
+myChart.options.animation = false;
+
+
+/*****
+  add event listener to html input fields
+*****/
+//  get all html form input fields so did not use array at the end
+let inputTexts = document.getElementsByClassName('form-group__textInput');
+
+//  loop to retrieve price and loan years input fields
+for (let i = 0; i < inputTexts.length; i++) {
+  //  this is NOT a click event listener but an input change event listener
+  //  so changes in the input field triggers this loop to occur and
+  //  triggers the updateInputsState function to capture the values put into the input fields
+  inputTexts[i].addEventListener('input', updateInputsState);
+}
+
+//  function to target input fields and assign the name and the value typed to variables
+function updateInputsState(event) {
+  let name = event.target.name;
+  let value = event.target.value;
+
+  //  change html input fields to actual numbers
+  if (name == 'price') {
+    value = getNumber(value);
+  }
+
+  state = {
+    //  grabs all values in the state object above and reassign it here
+    ...state,
+    [name]: value
+  }
+  console.log(state);
+}
+console.log(inputTexts);
